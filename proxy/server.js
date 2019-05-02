ea0f250c8ce89b30ea07ca8e944c5e59e01149ef
Cody Smith <cms287@gmail.com>
2019-05-02 14:40:13 -0700

    loader.io

--- proxy/server.js
+++ proxy/server.js
@@ -10,6 +10,11 @@ app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/albums/:id', express.static(path.join(__dirname, 'public')));
 
+app.get('/loaderio-8c90853dfafd879a8ca56f9fedf20e57', (req, res) => {
+  const filePath = path.join(__dirname, './loaderio-8c90853dfafd879a8ca56f9fedf20e57.txt')
+  res.sendFile(filePath);
+});
+
 app.listen(port, () => {
   console.log(`server running at port: ${port}`);
 });
